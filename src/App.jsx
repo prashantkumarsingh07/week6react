e471457a7b0bf1e7ab540303b9168fc05675dc98
Prashant Kumar Singh <prashantkumarsingh07pks@gmail.com>
2024-04-28 13:41:01 +0530

    Wrapper class

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,29 @@
-
- /// keys   whenever u are iteraring over an array u need unique id & a key
- // because it helps react to calculate  diffs  difference , and simplifies re -rendering logic.
-
-import { useState } from "react"
-
+// Learning wrapper class
 
 function App() {
-  
-  const [todos,setTodos] = useState([{
-    id:1,
-    title: "go t gym",
-    description: "go to  gym at 12 pm today",
-  },
-  {
-    id:2,
-    title: "go for cycling",
-    description: "go to  cycling  at 7 pm today",
-  },
-  {
-    id:3,
-    title: "go to party",
-    description: "go for  part  at 10 pm today",
-  }])
 
-  function addTodo(){
-    setTodos([...todos,{
-      id:4,
-      title:Math.random(),
-      description: Math.random(),
-    }])
-  }
-
- return <>
-      <button onClick={addTodo}>add a todo</button>
-     {todos.map(todo=><Todo key={todo.id} title={todo.title} description={todo.description} />)}
-  
-    </>
-    
-  
+  return (
+    <div style={{display: "flex"}}>
+      <Card>
+        hi there
+      </Card>
+      <Card>
+        <div>
+          hello from the 2nd card
+        </div>
+      </Card>
+    </div>
+  )
 }
 
-function Todo({title,description})
-{
-  return <div>
-        <h1>{title}</h1>
-        <h5>{description}</h5>
-   </div>
-  
+function Card({children}) {
+  return <div style={{
+    border: "1px solid black",
+    padding: 10,
+    margin: 10
+  }}>
+    {children}
+  </div>
 }
-
-
-
 
 export default App
-
-
