443e9911e7aedd4988f78f918922b231e702a6fe
Prashant Kumar Singh <prashantkumarsingh07pks@gmail.com>
2024-04-28 12:55:16 +0530

    Keys in react used while iterating map

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,56 @@
-  import React ,{Fragment} from "react";
-import {  useState } from "react"
 
-//  import React from "react";
+ /// keys   whenever u are iteraring over an array u need unique id & a key
+ // because it helps react to calculate  diffs  difference , and simplifies re -rendering logic.
+
+import { useState } from "react"
 
 
 function App() {
-  const [title,setTitle] = useState("my  name is prashant"); 
- 
- function updateTitle(){
-  setTitle("my name is "+ Math.random());
- }
   
- return (
-    <>
-    <button onClick={updateTitle}>click here to change title</button>
-    <Header title={title} />
-    <Header title="prashant1"/>
-    <Header title="prashant2"/>
-    <Header title="prashant3"/>
-    <Header title="prashant4"/>
-    <Header title="prashant2"/>
-    <Header title="prashant3"/>
+  const [todos,setTodos] = useState([{
+    id:1,
+    title: "go t gym",
+    description: "go to  gym at 12 pm today",
+  },
+  {
+    id:2,
+    title: "go for cycling",
+    description: "go to  cycling  at 7 pm today",
+  },
+  {
+    id:3,
+    title: "go to party",
+    description: "go for  part  at 10 pm today",
+  }])
+
+  function addTodo(){
+    setTodos([...todos,{
+      id:4,
+      title:Math.random(),
+      description: Math.random(),
+    }])
+  }
+
+ return <>
+      <button onClick={addTodo}>add a todo</button>
+     {todos.map(todo=><Todo key={todo.id} title={todo.title} description={todo.description} />)}
   
     </>
-  )
+    
+  
 }
 
-const Header = React.memo(function Header({title}){
+function Todo({title,description})
+{
   return <div>
-       {title}
-  </div>
-})
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+   </div>
+  
+}
+
+
+
 
 export default App
 
